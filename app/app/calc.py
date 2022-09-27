1375264bd3c1c5a332de54900a00acc5f0e1d285
almostdevelopr <minhalam.learn@gmail.com>
2022-09-27 14:29:17 +0530

    Add tests first and then code

--- app/app/calc.py
+++ app/app/calc.py
@@ -5,3 +5,7 @@ Calculator function.
 def add(x, y):
     """ Add x and y and returns result."""
     return x + y
+
+def subtract(x, y):
+    """Subtract y from x and returns result."""
+    return x - y
--- app/app/tests.py
+++ app/app/tests.py
@@ -9,8 +9,14 @@ from app import calc
 class CalcTests(SimpleTestCase):
     """ Test the calc module. """
 
-    def test_add_number(self):
+    def test_add_numbers(self):
         """Test adding numbers together."""
         res = calc.add(5,9)
 
         self.assertEqual(res, 14)
+
+    def test_subtract_numbers(self):
+        """Subtract numbers."""
+        res = calc.subtract(10, 5)
+
+        self.assertEqual(res, 5)

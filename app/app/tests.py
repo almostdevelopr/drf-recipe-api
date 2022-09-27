6cc2ee414382c8dbe7b8b5d5522e0cc8f5a4f477
almostdevelopr <minhalam.learn@gmail.com>
2022-09-27 14:35:46 +0530

    Fixed Linting

--- app/app/tests.py
+++ app/app/tests.py
@@ -10,13 +10,11 @@ from app import calc
 class CalcTests(SimpleTestCase):
     """ Test the calc module. """
 
-
     def test_add_numbers(self):
         """Test adding numbers together."""
         res = calc.add(5, 9)
 
         self.assertEqual(res, 14)
-
 
     def test_subtract_numbers(self):
         """Subtract numbers."""
